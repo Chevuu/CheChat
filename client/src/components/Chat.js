f869e20be370143b41b6d205eaec58339c1a4b16
Vuk <vukjurisic.tech@gmail.com>
2023-04-29 16:04:28 +0200

    private chatting possible

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -5,16 +5,22 @@ import "./styles/Chat.css";
 const Chat = () => {
   const [messages, setMessages] = useState([]);
   const ws = useRef(null);
+  const [recipientPort, setRecipientPort] = useState(null);
 
   useEffect(() => {
-    ws.current = new WebSocket('ws://localhost:8080/');
+    ws.current = new WebSocket(`ws://localhost:8080/`);
+    ws.current.onerror = (event) => {
+      console.error('WebSocket error:', event);
+    };
     ws.current.onmessage = (event) => {
       const message = event.data;
+      console.log("Received message" + message)
       const messageTime = new Date().toLocaleTimeString([], { hour12: false, hourCycle: 'h23', hour: '2-digit', minute: '2-digit' });
       const newMessage = {
         message: message.split(':')[0],
         time: messageTime,
-        senderPort: message.split(':')[1],
+        receiverPort: message.split(':')[1],
+        senderPort: message.split(':')[2],
       };
       setMessages((prevMessages) => [...prevMessages, newMessage]);
     };
@@ -31,10 +37,13 @@ const Chat = () => {
     if (message === "") {
       return;
     }
-    ws.current.send(`${message}:${window.location.port}`);
+    ws.current.send(`${message}:${recipientPort}:${window.location.port}`);
     event.target.reset();
   };
-  
+
+  const handleRecipientPortChange = (event) => {
+    setRecipientPort(event.target.value);
+  }
 
   return (
     <div className="chat-container">
@@ -48,6 +57,14 @@ const Chat = () => {
           name="message"
           placeholder="Type a message..."
           autoComplete="off"
+        />
+        <input
+          className="chat-recipient-port"
+          type="text"
+          name="recipient-port"
+          placeholder="Port"
+          autoComplete="off"
+          onChange={handleRecipientPortChange}
         />
         <button className="chat-submit" type="submit">
           Send
